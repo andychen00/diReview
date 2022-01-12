9b7d3b1aea5265f3945c07fd62818f531f77e34f
Zenxiao <searaid123@gmail.com>
2022-01-12 22:50:18 +0700

    hard coded Detail Activity view

--- app/src/main/java/andy/com/direview/DetailAct.java
+++ app/src/main/java/andy/com/direview/DetailAct.java
@@ -2,21 +2,36 @@ package andy.com.direview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class DetailAct extends AppCompatActivity {
+
+    private ImageView mImageView;
+    private Button reviewBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        init();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back);
+
+        mImageView.setImageResource(R.drawable.android_nums);
+    }
+
+    public void init(){
+        mImageView = findViewById(R.id.productView);
+        reviewBtn = findViewById(R.id.reviewBtn);
     }
 
     @Override
@@ -24,5 +39,10 @@ public class DetailAct extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    public void backToHome(View view) {
+        Intent intent = new Intent(DetailAct.this, MainActivity.class);
+        startActivity(intent);
     }
 }
