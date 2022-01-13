7d27cce4018be6959c29b0c46427e62901e7c8dc
Juan A <juan.as@yahoo.com>
2022-01-13 21:12:59 +0700

    splash screen & login (masih ongoing)

---
+++ app/src/main/java/andy/com/direview/SplashActivity.java
@@ -0,0 +1,20 @@
+package andy.com.direview;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
+    }
+}
