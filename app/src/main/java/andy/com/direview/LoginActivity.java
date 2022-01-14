42c0fc37f21a46650ed504a852c8bcb03d65c6b4
Juan A <juan.as@yahoo.com>
2022-01-14 17:27:20 +0700

    login btn error (listener)

--- app/src/main/java/andy/com/direview/LoginActivity.java
+++ app/src/main/java/andy/com/direview/LoginActivity.java
@@ -6,12 +6,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-    public LoginActivity(Intent intent) {
 
-    }
 
 //    private static final String[] DUMMY_ACCOUNT = new String[]{
 //            "foo@example.com:hello", "bar@example.com:world"
@@ -24,23 +23,23 @@ public class LoginActivity extends AppCompatActivity {
 
         EditText userEmail = (EditText) findViewById(R.id.tfEmail);
         EditText  userPassword = (EditText) findViewById(R.id.tfPassword);
-        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        Button clickLogin = (Button) findViewById(R.id.btnLogin);
 
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        clickLogin.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                String UserMail = userEmail.getText().toString();
-                String UserPwd = userPassword.getText().toString();
-                if(UserMail.equalsIgnoreCase("kelompok3@binus.ac.id") && UserPwd.equals("password")){
-                    Intent MainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                String UserName = userEmail.getText().toString();
+                String Pwd = userPassword.getText().toString();
+                if(UserName.equalsIgnoreCase("kelompok3@binus.ac.id") && Pwd.equals("password")){
+                    Intent MainIntent = new Intent(LoginActivity.this, CategoryActivity.class);
                     startActivity(MainIntent);
-                    Toast.makeText(LoginActivity.this,"You are Sign in, Welcome.", Toast.LENGTH_LONG).show();
+                    Toast.makeText(LoginActivity.this,"You are Signed in.", Toast.LENGTH_LONG).show();
                 }else
                 {
                     Toast.makeText(LoginActivity.this,"Email or Password is incorrect.", Toast.LENGTH_LONG).show();
                 }
             }
+
         });
 
     }
